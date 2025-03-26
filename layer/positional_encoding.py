d5c5811ab4fcce2e58cacdccf279ab4b442662a2
HuaTanSang <huatansang2004@gmail.com>
2025-03-26 22:06:55 +0700

    fix loop v2

--- layer/positional_encoding.py
+++ layer/positional_encoding.py
@@ -17,7 +17,7 @@ class PositionalEncoding(nn.Module):
         for batch in batch_size:
             for position in range(0, num_seq, 2): 
                 pe[batch][position] = math.sin(position / (10000**(2*position/embedding)))
-                pe[batch][position] = math.cos(position / (10000**(2*(position+1)/embedding_dim)))
+                pe[batch][position+1] = math.cos((position+1) / (10000**(2*(position+1)/embedding_dim)))
     
         return pe + embedding
     
