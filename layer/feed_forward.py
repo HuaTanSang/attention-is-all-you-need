0ed10f77d159f0f689830b7a382fdd58ff2a1f0b
HuaTanSang <huatansang2004@gmail.com>
2025-03-26 21:03:33 +0700

    update drop out

--- layer/feed_forward.py
+++ layer/feed_forward.py
@@ -1,7 +1,7 @@
 import torch.nn as nn 
 
 class FC_FeedForwardnetwork(nn.Module): 
-    def __init__(self, d_model, d_hidden): 
+    def __init__(self, d_model, d_hidden, drop_out=0.2): 
         super(FC_FeedForwardnetwork, self).__init__() 
         self.d_model = d_model 
         self.d_hidden = d_hidden
@@ -10,10 +10,12 @@ class FC_FeedForwardnetwork(nn.Module):
         self.second_linear = nn.Linear(self.d_hidden, self.d_model) 
 
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_out)
 
     def forward(self, X): 
         output = self.first_linear(X) 
         output = self.relu(output)
+        output = self.dropout(output)
         output = self.second_linear(output) 
 
         return output 
