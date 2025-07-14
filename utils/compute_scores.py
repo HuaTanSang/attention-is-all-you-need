d76f75f40877294deb4f36f17fccb47ccb776746
HuaTanSang <huatansang2004@gmail.com>
2025-07-14 09:17:17 +0700

    change utils location

---
+++ utils/compute_scores.py
@@ -0,0 +1,16 @@
+import torch 
+import torch.nn as nn 
+
+
+def BLEU(true_sentencens: str, pred_senteces: str): 
+
+
+    return 
+
+
+
+def EM(true_sentences: str, pred_sentences: str): 
+
+    return 
+
+
