3dde8e90c279eea8c6301bc80d4c0020e976ca1d
HuaTanSang <huatansang2004@gmail.com>
2025-09-17 09:10:30 +0700

    add compute score

--- utils/compute_scores.py
+++ utils/compute_scores.py
@@ -1,16 +1,15 @@
-import torch 
-import torch.nn as nn 
+from typing import List 
+from torchmetrics.functional.text.ter import ter
+
+from torchtext.data.metrics import bleu_score
+
+def BLEU(candidate_corpus: List[str], references_corpus: List[str]): 
+    score = bleu_score(candidate_corpus=candidate_corpus, 
+                       references_corpus=references_corpus)
+    return score
 
 
-def BLEU(true_sentencens: str, pred_senteces: str): 
-
-
-    return 
-
-
-
-def EM(true_sentences: str, pred_sentences: str): 
-
-    return 
-
-
+def TER(candidate_corpus: List[str], references_corpus: List[str]): 
+    score = ter(candidate_corpus=candidate_corpus, 
+                references_corpus=references_corpus)
+    return score 
